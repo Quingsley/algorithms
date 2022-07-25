8c463c1dd565ef701321bf381b309ba4e88e88d6
JEROME JUMAH <74877189+Quingsley@users.noreply.github.com>
2022-07-25 09:29:08 +0300

    fixed issue on number of level in binary tree

--- numOfLevelsBinaryTree.js
+++ numOfLevelsBinaryTree.js
@@ -9,12 +9,33 @@ class Node {
 }
 
 const countLevels = function (node) {
-  let levels = 0;
-  if (node === null) return levels;
-  if (node.left === null && node.right === null) {
-    levels = 1;
+  debugger;
+  let numOfLevels;
+  if (node === null) {
+    numOfLevels = 0;
   } else {
+    let left = countLevels(node.left);
+    let right = countLevels(node.right);
+    numOfLevels = left > right ? left + 1 : right + 1;
   }
 
-  return levels;
+  return numOfLevels;
 };
+
+function newNode(data) {
+  let node = new Node(data);
+  return node;
+}
+
+/*create a tree*/
+let root = newNode(4);
+root.left = newNode(3);
+root.right = newNode(11);
+root.left.right = newNode(5);
+root.left.right.left = newNode(9);
+root.left.right.left.right = newNode(10);
+root.left.right.left.right.left = newNode(15);
+root.right.right = newNode(8);
+root.right.right.right = newNode(6);
+/*get leaf count of the above created tree*/
+console.log("The number of levels in the binary tree is: " + countLevels(root));
